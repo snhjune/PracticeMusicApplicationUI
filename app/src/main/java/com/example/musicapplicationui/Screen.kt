2d73ea081143b7a28d9b17367a2a53a3e95586a6
Sunnyong Hur <79673305+snhjune@users.noreply.github.com>
2024-03-15 17:29:20 -0700

    "Initial commit "

--- app/src/main/java/com/example/musicapplicationui/Screen.kt
+++ app/src/main/java/com/example/musicapplicationui/Screen.kt
@@ -3,7 +3,14 @@ package com.example.musicapplicationui
 import androidx.annotation.DrawableRes
 
 sealed class Screen(val title: String, val route: String) {
+    sealed class BottomScreen(
+        val bTitle: String, val bRoute: String, @DrawableRes val icon: Int
+    ): Screen(bTitle,bRoute){
+        object Home : BottomScreen("Home","home", R.drawable.baseline_music_video_24)
+        object Liabrary: BottomScreen("Library", "library", R.drawable.baseline_library_music_24)
+        object Browse: BottomScreen("Browse", "browse", R.drawable.baseline_apps_24)
 
+    }
     sealed class DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes val icon: Int)
         : Screen(dTitle, dRoute){
             object Account: DrawerScreen(
@@ -23,7 +30,11 @@ sealed class Screen(val title: String, val route: String) {
             )
         }
 }
-
+val screensInBottom = listOf(
+    Screen.BottomScreen.Home,
+    Screen.BottomScreen.Browse,
+    Screen.BottomScreen.Liabrary
+)
 val screensInDrawer = listOf(
     Screen.DrawerScreen.Account,
     Screen.DrawerScreen.Subscription,
